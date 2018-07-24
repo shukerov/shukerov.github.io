b8f0e37bc4f61d584711f367d3fd561d64f61cf8
Stoyan Shukerov <stoichik@gmail.com>
2018-07-24 15:24:46 +0300

    moving project to gitlab. Setting up autodeploy

--- config.rb
+++ config.rb
@@ -7,15 +7,6 @@ page '/*.txt', layout: false
 activate :directory_indexes
 activate :livereload
 
-# github deploy
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.build_before = true
-  deploy.branch = "master"
-  deploy.remote = "git@github.com:shukerov/shukerov.github.io.git"
-  deploy.commit_message = "Delpoying @ #{Time.now}"
-end
-
 # markdown and syntax highlighting
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
@@ -29,9 +20,9 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
+  set :build_dir, 'public'
+  set :base_url, "/myblog" # baseurl for GitLab Pages (project name) - leave empty if you're building a user/group website
+  activate :relative_assets # Use relative URLs
 end
