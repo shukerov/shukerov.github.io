fae2a355b7ffabef8c9f2c14dbb958bb742481a2
Stoyan Shukerov <stoichik@gmail.com>
2020-02-10 22:26:54 -0800

    Active page is marked in the navbar.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,14 @@
-// This is where it all goes :)
+console.log('Hi 👋.');
+console.log('Find the source code for this website here:');
+console.log('https://github.com/shukerov/shukerov.github.io');
+
+markActiveTab();
+
+// active tab
+function markActiveTab() {
+  const mainContent = document.getElementsByTagName('main')[0];
+  const activeNavItemName = `nav-item-${mainContent.id}`;
+  const activeNavItem = document.getElementById(activeNavItemName);
+
+  activeNavItem.classList.add('nav-active-item');
+}
